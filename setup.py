4f10a311d5327b149e53b84407c7cc721b84584b
kangbobi <devprogramming.bs@gmail.com>
2025-07-07 16:34:40 +0700

    upd

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in frappe_whatsapp/__init__.py
-from frappe_whatsapp import __version__ as version
+from frappe_telegraf_ui import __version__ as version
 
 setup(
     name="frappe_telegraf_ui",
